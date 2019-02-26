e2dcbd04e9be858ee1f1e4c3622bd82470be4d80
Alejandra Aravena <janaravena@gmail.com>
2019-02-25 23:08:54 -0300

    m3 paso 4

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Socket } from 'ng-socket-io';
+
 /**
  * Generated class for the ChatPage page.
  *
@@ -17,11 +19,19 @@ export class ChatPage {
 
 	tituloPagina: string = "Chatbox";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	nickname = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private socket: Socket) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChatPage');
+  }
+
+  unirseAlChat() {
+    this.socket.connect();
+    this.socket.emit('set-nickname', this.nickname);
+    this.navCtrl.push('ChatRoomPage', { nickname: this.nickname });
   }
 
 }
